2eecf2f4a8629a629f6d8e6d895a965627467430
RondoPuffin <dachronosduck@gmail.com>
2025-04-26 03:10:15 -0700

    Initial Commit

---
+++ Cosmoclub/CosmoclubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmoclubApp.swift
+//  Cosmoclub
+//
+//  Created by Admin on 26/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct CosmoclubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CosmoclubTests/CosmoclubTests.swift
@@ -0,0 +1,17 @@
+//
+//  CosmoclubTests.swift
+//  CosmoclubTests
+//
+//  Created by Admin on 26/4/2568 BE.
+//
+
+import Testing
+@testable import Cosmoclub
+
+struct CosmoclubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
