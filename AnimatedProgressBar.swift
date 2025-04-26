782a08603128e1c1834d33e7be27204536a19636
RondoPuffin <dachronosduck@gmail.com>
2025-04-26 03:12:53 -0700

    Set

---
+++ AnimatedProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedProgressBar.swift
+//  Cosmoclub
+//
+//  Created by Admin on 26/4/2568 BE.
+//
+
+import SwiftUI
+
+struct AnimatedProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedProgressBar()
+}
